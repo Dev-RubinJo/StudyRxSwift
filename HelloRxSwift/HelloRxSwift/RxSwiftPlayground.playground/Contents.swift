8ac4ea8dda794c263e8dc5df5f2d85723323a489
Rubin.Jo <dbqlsdl1234@naver.com>
2021-02-01 22:29:50 +0900

    RxSwift Publish, Behavior, Replay, BehaviorRelay에 대해 공부 Variable은 최신버전에서 삭제되고 BehaviorRelay가 그 역할을 대체

--- HelloRxSwift/HelloRxSwift/RxSwiftPlayground.playground/Contents.swift
+++ HelloRxSwift/HelloRxSwift/RxSwiftPlayground.playground/Contents.swift
@@ -1,24 +1,62 @@
 import UIKit
 import RxSwift
+import RxCocoa
 
 let disposeBag = DisposeBag()
 
-Observable.of("A", "B", "C")
+//let subject = PublishSubject<String>()
+//
+//subject.onNext("Issue 1")
+//
+//subject.subscribe { event in
+//    print(event)
+//}
+//
+//subject.onNext("Issue 2")
+//subject.onNext("Issue 3")
+//
+//subject.dispose()
+//subject.onCompleted()
+//subject.onNext("Issue 4")
+
+
+//let subject = BehaviorSubject(value: "Initial Value")
+//
+//subject.onNext("Last Issue")
+//
+//subject.subscribe { event in
+//    print(event)
+//}
+//
+//subject.onNext("Issue 1")
+
+//let subject = ReplaySubject<String>.create(bufferSize: 2)
+//subject.onNext("Issue 1")
+//subject.onNext("Issue 2")
+//subject.onNext("Issue 3")
+//
+//subject.subscribe {
+//    print($0)
+//}
+//
+//subject.onNext("Issue 4")
+//subject.onNext("Issue 5")
+//subject.onNext("Issue 6")
+//
+//print("[Subscription 2]")
+//subject.subscribe {
+//    print($0)
+//}
+
+let relay = BehaviorRelay(value: [String]())
+relay.accept(["Item 1"])
+
+var value = relay.value
+value.append("Item 2")
+value.append("Item 3")
+relay.accept(value)
+relay.asObservable()
     .subscribe {
         print($0)
-    }.disposed(by: disposeBag)
-
-Observable<String>.create { observer in
-    observer.onNext("A")
-    observer.onCompleted()
-    observer.onNext("?")
-    return Disposables.create()
-}.subscribe (onNext: {
-    print($0)
-}, onError: {
-    print($0)
-}, onCompleted: {
-    print("Completed")
-}, onDisposed: {
-    print("Disposed")
-})
+    }
+relay.accept(relay.value + ["Item 2"])
